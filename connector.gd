392289bc8e34ec2d3d9af75e251d4c64e04e82f9
Josh Sarver <josh.sarver@gmail.com>
2024-08-23 00:07:57 -0700

    moved some of the logic around, multiple lines now work

--- connector.gd
+++ connector.gd
@@ -3,43 +3,56 @@ extends Node2D
 var point1 : Vector2 = Vector2(0, 0)
 var width : int = 10
 var color : Color = Color.GREEN
-var _point2 : Vector2 
 var start: Vector2
-var stop: Vector2
-var line = preload('res://dotline.tscn')
+var end: Vector2
+var current_line : Line2D
+
 
 	#print(line)
-func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var parent = get_node('/root/main')
-		var mouse_position = get_viewport().get_mouse_position()
-		point1 = parent.dot_position
-		#print('in the line!')
-		
-		if mouse_position != _point2:
-			_point2 = mouse_position
-			queue_redraw()
+#func _process(delta):
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		#var end = get_viewport().get_mouse_position()
+		#
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 
-func _draw():
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		draw_line(point1, _point2, color, width)
-	else:
-		var new_line = line.instantiate()
-		stop = position
-		new_line = Line2D.new()
-		new_line.points = PackedVector2Array( [start, stop])
-		
-		add_child(new_line)
-func _on_dot_end_connection(position):
-	#queue_redraw()
+		#var mouse_position = get_viewport().get_mouse_position()
+		#point1 = parent.dot_position
+		##print('in the line!')
+		#
+		#if mouse_position != _point2:
+			#_point2 = mouse_position
+			#queue_redraw()
+#
+#func _draw():
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		#draw_line(start, _point2, color, width)
+	#else:
+		##var new_line = line.instantiate()
+		##end = position
+		##new_line = Line2D.new()
+		##new_line.points = PackedVector2Array( [start, end])
+		##
+		##add_child(new_line)
+#func _on_dot_end_connection(position):
+	##queue_redraw()
+	#end = position
+	#current_line.points = PackedVector2Array([start,end])
+	##parent.add_child(current_line)
+	#print('child added')
+	#
+#
+#func _on_dot_start_connection(position):
+	#var parent = get_node('/root/main')
+	#current_line = Line2D.new()
+	#print(current_line.get_parent())
+	#add_child(current_line)
+	#print('start connection',position)
+	#
+	#print(current_line.get_parent())
+	#start = position
+	#end = position
+	#current_line.add_point(start)
+	#current_line.add_point(end)
 
-	print('child added')
-	
-
-func _on_dot_start_connection(position):
-	print('start connection',position)
-
-	start = position
-	
 	
 
--- game.gd
+++ game.gd
@@ -5,16 +5,14 @@ var connection = connector.instantiate()
 @export var columns :int 
 @export var rows : int 
 @export var dot_distance:int = 150
-var dot_position : Vector2
-
-
+var current_line : Line2D
+var start : Vector2
+var end : Vector2
 #
 #func setup_game():
 	#for "res://dot.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-
 	var x_pos = 150
 	var y_pos = 150
 	for c in range(columns):
@@ -24,11 +22,32 @@ func _ready():
 			#var connection = connector.instantiate()
 			#add_child(connection)
 			next_dot.position = Vector2(x_pos,y_pos)
-			next_dot.start_connection.connect(connection._on_dot_start_connection)
-			next_dot.end_connection.connect(connection._on_dot_end_connection)
+			next_dot.start_connection.connect(_on_dot_start_connection)
+			next_dot.end_connection.connect(_on_dot_end_connection)
 			add_child(next_dot)
 			y_pos += dot_distance
 		x_pos += dot_distance
 		y_pos = dot_distance
 
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if not current_line:
+			pass
+		else:
+			current_line.set_point_position(1, get_viewport().get_mouse_position())
+			print('drawing',end)
+		
 
+func _on_dot_end_connection(position):
+	current_line.set_point_position(1, position)
+	
+	
+
+func _on_dot_start_connection(position):
+	current_line = Line2D.new()
+	add_child(current_line)
+	print('start connection',position)
+	start = position
+	end = position
+	current_line.points= PackedVector2Array([start,end])
+	
